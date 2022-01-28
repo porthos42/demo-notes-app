ed4a90eb81de5ff2454e20f61d8daca689a7fd81
porthos42 <philopolis42@gmail.com>
2022-01-28 10:42:03 -1000

    Adding a storage stack

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -4,11 +4,17 @@ export default class StorageStack extends sst.Stack {
     // Public reference to the table
     table;
 
+    // Public reference to the bucket
+    bucket;
+
     constructor(scope, id, props) {
         super(scope, id, props);
         
+        // Create an S3 bucket
+        this.bucket = new sst.Bucket(this, "Uploads");
+
         // Create the DynamoDB table
-        this.table = new sst.Table(this, "NotesV2", {
+        this.table = new sst.Table(this, "Notes", {
             fields: {
                 userId: sst.TableFieldType.STRING,
                 noteId: sst.TableFieldType.STRING,
